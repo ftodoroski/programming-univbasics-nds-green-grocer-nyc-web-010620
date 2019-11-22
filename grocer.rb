a5721884b9277c196a9d41ea91f27903b9501ed7
Filip Todoroski <ftodoroski@ymail.com>
2019-11-22 18:28:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def consolidate_cart(cart)
   i = 0
   while i < cart.length
     item = cart[i]
-    item_name = cart[i][:iten]
+    item_name = cart[i][:item]
 
     if find_item_by_name_in_collection(item_name, basket)
       basket_item = find_item_by_name_in_collection(item_name, basket)
