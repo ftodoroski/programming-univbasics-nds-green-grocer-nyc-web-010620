6fa8701e9342e74ca756d84d5462903ade6cead4
Filip Todoroski <ftodoroski@ymail.com>
2019-11-22 17:21:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,6 +17,7 @@ def get_uniq(cart)
 
   i = 0
   while i < cart.length
+    item_name = cart[i][:name]
     item = cart[i]
 
     if find_item_by_name_in_collection(item_name, basket) == nil
@@ -31,7 +32,8 @@ end
 
 
 def consolidate_cart(cart)
-  pp get_uniq(cart)
+  # pp get_uniq(cart)
+
 
 end
 
